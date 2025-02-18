61df9a252084857825b68ea2a091781a6bfeb118
Imran <imranmatloob@live.co.uk>
2025-02-18 18:27:48 +0000

    building an linkedList

--- main.c
+++ main.c
@@ -1,33 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    int arr[] = {1, 2, 3, 4, 6};
-    int l = sizeof(arr)/ sizeof(arr[0]);
-
-
-    int a = arr[0];
-    int b = arr[0];
-
-    for(int i = 0; i <= l -1; i++){
-        if(a >= arr[i]){
-            a = arr[i];
-        }
-        if(b <= arr[i]){
-            b = arr[i];
-        }
-    }
-    printf("%d\n", a);
-    printf("%d\n", b);
-
-    for(int j = 0; j <= l-1; j++){
-        while(j <= l){
-            if(a != arr[j]){
-                printf("%d",a);
-            }
-            a++;
-            break;
-        }
-    }
 
     return 0;
 }
+
+typedef struct node node;
+struct node {
+    int data;
+    node* next;
+};
+
+node* CreateHead(int data){
+    node* a = (node *) malloc( sizeof(node));
+    a->data = data;
+    a->next = NULL;
+    return a;
+}
+
+node* insertAtBeginning (node* current_head,int data){
+    node* new_node = (node*) malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = current_head;
+    return new_node;
+}
+
+node* insertAtEnd(node* current_head, int data){
+    node* a = (node*) malloc(sizeof(node));
+    a->data = data;
+    a->next = NULL;
+    node* tempHead = current_head;
+    while(tempHead->next != NULL){
+        tempHead = tempHead->next;
+        if (tempHead->next == NULL){
+            tempHead->next = a;
+        }
+    }
+    return a;
+}
+
+node* traverseList(node* current_head, int data){
+    node* temp = current_head;
+    while (temp != NULL){
+        if(temp->data == data){
+            return temp;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+node* deleteNode(node* head, int delete){
+    node* prev = head;
+    node* curr = head->next;
+
+    if(prev->data == delete){
+        free(prev);
+        prev = curr;
+        curr = curr->next;
+    }
+    while(curr != NULL){
+        if(curr->data == delete){
+            prev->next = curr->next;
+            free(curr);
+            curr = prev->next;
+        } else{
+            prev = prev->next;
+            curr = curr->next;
+        }
+    }
+    return prev;
+
+}
+
+
