e7f4f728a1539c1fe92acb3bb17683954552bb09
Imran <imranmatloob@live.co.uk>
2025-03-07 16:17:50 +0000

    Arrays

--- main.c
+++ main.c
@@ -2,96 +2,59 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <assert.h>
 
-struct node{
-    char* key;
-    char* value;
-    struct node* next;
-};
-
-void setNode(struct node* node, char* key, char* value){
-    node->key = key;
-    node->value = value
-    node->next = NULL;
-    return;
+void insert(int a[], int *e, int val){
+    a[*e] = val;
+    (*e)++;
 }
 
-struct hashMap{
-    int size, eleNUmber;
-    struct node** arr;
-};
+int removeDuplicates(int* nums, int numsSize) {
+ int *a = nums;
+ int aSize = numsSize;
+ int b[aSize];
+ int i = 1;
+ int e = 1;
+ b[0] = a[0];
 
-void initizseHashMap(struct hashMap* nash){
-    nash->size = 100;
-    nash->eleNUmber=0;
-    nash->arr = (struct node**)malloc(sizeof(node*) * size);
-    return;
-}
-
-int hashFunction(struct hashMap* hash, char* value){
-    //get length of string =a,
-    int a = strlen(value);
-    // empty int actualIndex = 0
-    int actualIndex = 0;
-    //empty varible that holds ints for anki addup = sum
-    int sum = 0;
-    for(int i = 0; i =< a-1; i++){
-        //1, get the first char = b
-        //2, get aski values of that char
-        int ascii = (int)value[i];
-        //3, add that value to sum,
-        sum = sum + ascii;
-        //4, a--
+while(i < aSize){
+    int t = a[i-1];
+    if(t != a[i]){
+        insert(b,&e,a[i]);
     }
-    //5, do 1,2,3,4 while a !=0;
-    actualIndex = sum % hash->size;
-    // actualIndex = sum%hash->size
-    return actualIndex;
-    //return actualIndex
+    i++;
+}
+int c[e];
+int j=0;
+while(j<e){
+    c[j] = b[j];
+    j++;
 }
 
-void insert(struct hashMap* hash, char* value){
-    a = hashFunction(hash,value);
-    node* b = (struct node*) malloc(sizeof(struct node));
-    setNode(b,a,value);
-    if(hash->arr[a] == NULL){
-        hash->arr[a] = b;
-    }else if(hash->arr[a] != NULL){
-        b->next = arr[a];
-        arr[a] = b;
-    }
-    return;
-}
-
-void delete(struct hashMap* hash, char* key){
-    int a = hashFunction(hash, key);
-    if (hash->arr[a] != NULL){
-
-        struct node* head = hash->arr[a];
-        struct node* next = head->next;
-
-        //whilst head is not null
-        while(head != NULL){
-            //if head is equal to the value, set arr to next and free head
-            if(strcmp(head->key, key) == 0){
-                hash->arr[a] = next;
-                //else if next value is equal to value, than set head's next value to nexts next value,
-                //free next, and assign a the new value to next.
-            }else if(next != NULL){
-                if(strcmp(next->key, key) == 0){
-                    head->next = next->next;
-                    next = head->next;
-                }
-            }
-            head = next;
-            next = head->next;
-        }
-    }
-    return;
+    return *c;
 }
 
 int main() {
 
+
+    // Input array (sorted array with duplicates)
+    int nums[] = {1, 1, 2, 2, 3, 4, 4, 5};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+
+    // Expected array after duplicates are removed
+    int expectedNums[] = {1, 2, 3, 4, 5};
+    int expectedSize = sizeof(expectedNums) / sizeof(expectedNums[0]);
+
+    // Call your function
+    int k = removeDuplicates(nums, numsSize);
+
+
+    // Test that the first k elements match the expected array
+    for (int i = 0; i < k; i++) {
+        assert(nums[i] == expectedNums[i]);
+    }
+
+    printf("All tests passed! k = %d\n", k);
     return 0;
 }
 
