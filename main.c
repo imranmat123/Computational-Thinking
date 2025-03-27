6738ae5a6ef02fdd17232055ea3edbe0538d9293
Imran Matloob <imranmatloob@live.co.uk>
2025-03-27 15:51:13 +0000

    learning about memory in C

--- main.c
+++ main.c
@@ -1,58 +1,114 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <assert.h>
-#include <ctype.h>
-#include <time.h>
-#include <math.h>
 
-#define LOWER  0
-#define UPPER 300
-#define STEP 20
-float convertToC(float f){
-  float c = (5.0/9.0) * (f-32.0) ;
-  return c;
+
+// int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
+//
+//     *returnSize =2;
+//     int* r = malloc(2 * sizeof(int));
+//     //{2,7,11,15}
+//     int i =0;
+//     int e = numbersSize;
+//     int start = numbers[i];
+//     int end = numbers[e];
+//     int j = 0;
+//
+//     while (j != e) {
+//
+//         if (start + end == target) {
+//                 r[0] = i +1;
+//                 r[1] = e +1;
+//             return r;
+//         }
+//
+//         if (end > target) {
+//             end = numbers[e -= 1];
+//         }else if (start + end > target) {
+//             start = numbers[+1];
+//             end = numbers[e -=1];
+//         }
+//         j++;
+//     }
+//     return 0;
+//
+// }
+//
+// char *get_temperature_status(int temp){
+//     if(temp > 90){
+//         printf("too hot");
+//     }else if(temp < 70){
+//         printf("too cold");
+//     }else{
+//         printf("just right");
+//     }
+// }
+//
+// struct Coordinate {
+//     int x;
+//     int y;
+//     int z;
+// };
+//
+// struct Coordinate new_coord(int x, int y, int z) {
+//     struct Coordinate c = {
+//         .x = x,
+//         .y = y,
+//         .z = z
+//     };
+//     return c;
+// }
+
+// bool isValid(char* s) {
+//     char* stack[strlen(s)];
+//     int top = -1;
+//     for (int i = 0; s[i] != '\0'; i++) {
+//         printf("%s",s);
+//
+//         if (s[i] == '('|| s[i] == '{' || s[i] == '[' ) {
+//              *stack[top] =  s[i];
+//             top++;
+//             printf("%s", stack[top]);
+//         }
+//     }
+// }
+
+
+typedef struct num{
+    int x;
+    int y;
+    int z;
+}num1;
+
+// num1 num_123(num1* f, int new_num) {
+//     num1 new_f = *f;
+//     printf("%p",new_f);
+//     // printf("%d", (*f).x);
+//     // printf("%d", (*f).y);
+//     // (*f).x = new_num;
+//     // return *f;
+// }
+
+void concat_strings(char *str1, const char *str2) {
+  //iterate over str1 until we find '/O'
+
+    int a = 0;
+    for(int i = 0; str1[i] != '\0'; i++) {
+        a++;
+    }
+    for (int i = 0; str2[i] != '\0'; i++) {
+        str1[a+i] = str2[i];
+    }
 }
-
-float convertToF(float c) {
-    float f = c *9.0/5.0 + 32.0;
-    return f;
-}
-
 
 
 int main() {
-    puts("you will give me a newline?");
-    printf("F to C %f\n", convertToC(100.00));
-    printf("C to F %f\n", convertToF(100));
-
-    float f = 0.0;
-    float c = 0.0;
-    float update = 0.0;
-
-    //going in assending order
-    do{
-        printf("Converting Fah to Cel %2.0f ", convertToC(f));
-        printf("Converting Cel to Fah %2.0f\n" , convertToF(c));
-
-        update += STEP;
-        c = update; f = update;
-    }while(update < UPPER);
-
-    //going in desending order
-
-    puts("Desenidng order with a forloop:");
-    for (update = UPPER; update > 0; update -= STEP) {
-        c = update; f = update;
-        printf("Converting C to F %2.0f ",convertToF(c));
-        printf("Converting F to C %2.0f\n", convertToC(f));
-    }
+  char str1[100] = "Hello ";
+  const char *str2 = "World";
+  concat_strings(str1, str2);
     return 0;
 }
-
-
-
 
 
 
